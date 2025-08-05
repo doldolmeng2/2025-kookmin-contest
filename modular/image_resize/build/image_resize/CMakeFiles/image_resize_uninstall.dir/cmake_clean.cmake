c3522bc82ff727a7242d21a286443342d48a2537
doldolmeng2 <ktypet13@hanyang.ac.kr>
2025-08-05 13:44:42 +0900

    build delete

--- modular/image_resize/build/image_resize/CMakeFiles/image_resize_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/image_resize_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/image_resize_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
